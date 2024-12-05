df0fd510c9e9b3ffadf728a11c6444aacebf7a35
aldalddl <aldalddl2007@gmail.com>
2024-12-05 16:26:37 +0900

    chore: 프로젝트 생성 및 gitignore 설정

---
+++ Cocoa Speech Synthesizer/Cocoa Speech Synthesizer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Cocoa Speech Synthesizer
+//
+//  Created by 강민지 on 12/5/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Cocoa Speech Synthesizer/Cocoa Speech Synthesizer/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Cocoa Speech Synthesizer
+//
+//  Created by 강민지 on 12/5/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
