547d3fe280e910080a21ff1401f9eb600eeed606
aldalddl <aldalddl2007@gmail.com>
2024-12-06 21:19:10 +0900

    feat: 프로젝트 생성

---
+++ Cocoa Slider Binding/Cocoa Slider Binding/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Cocoa Slider Binding
+//
+//  Created by 강민지 on 12/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Cocoa Slider Binding/Cocoa Slider Binding/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Cocoa Slider Binding
+//
+//  Created by 강민지 on 12/6/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
